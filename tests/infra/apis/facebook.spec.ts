d5a4732475838ba3a4e2628d2c5c64a9f7b5596f
João Victor <joaovictorslf@gmail.com>
2021-11-30 16:07:47 -0300

    ensure FacebookApi get debug token

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -15,17 +15,31 @@ describe('FacebookApi', () => {
   })
 
   beforeEach(() => {
+    httpClient.get.mockResolvedValueOnce({ access_token: 'any-app-token' })
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
 
   test('should get app token', async () => {
     await sut.loadUser({ token: 'any-client-token' })
+
     expect(httpClient.get).toHaveBeenCalledWith({
       url: 'https://graph.facebook.com/oauth/access_token',
       params: {
         client_id: clientId,
         client_secret: clientSecret,
         grant_type: 'client_credentials'
+      }
+    })
+  })
+
+  test('should get debug token', async () => {
+    await sut.loadUser({ token: 'any-client-token' })
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/debug_token',
+      params: {
+        access_token: 'any-app-token',
+        input_token: 'any-client-token'
       }
     })
   })
