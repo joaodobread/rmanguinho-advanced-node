be5464e2ec3b8e47e9ee9cf68489e5df76cea186
João Victor <joaovictorslf@gmail.com>
2021-11-30 11:39:22 -0300

    move LoadFacebookUserApi to its own file

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,17 +1,6 @@
+import { LoadFacebookUserApi } from '@/data/contracts/apis'
 import { FacebookAuthentication } from '@/domain/features'
 import { AuthenticationError } from '@/domain/models/errors'
-
-interface LoadFacebookUserApi {
-  loadUser: (params: LoadFacebookUserApi.Params) => Promise<LoadFacebookUserApi.Result>
-}
-
-namespace LoadFacebookUserApi {
-  export type Params = {
-    token: string
-  }
-
-  export type Result = undefined
-}
 
 class FacebookAuthenticationService {
   constructor (private readonly loadFacebookUserByTokenApi: LoadFacebookUserApi) {}
