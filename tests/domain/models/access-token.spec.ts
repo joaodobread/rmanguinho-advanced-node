b12d3e0bab445c2fadfad1e020bcb09f543aa3f9
João Victor <joaovictorslf@gmail.com>
2021-11-30 15:17:10 -0300

    ensure AccessToken expires in 30 min

--- tests/domain/models/access-token.spec.ts
+++ tests/domain/models/access-token.spec.ts
@@ -5,4 +5,8 @@ describe('AccessToken', () => {
     const sut = new AccessToken('any-value')
     expect(sut).toEqual({ value: 'any-value' })
   })
+
+  test('should expire in 1800000 ms', () => {
+    expect(AccessToken.expirationInMs).toBe(1800000)
+  })
 })
