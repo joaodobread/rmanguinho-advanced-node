59ac2f03fe6497d31f724f4bc7155466b8dd59ee
João Victor <joaovictorslf@gmail.com>
2021-11-30 14:21:03 -0300

    move business logic to a domain entity

---
+++ tests/domain/models/facebook-account.spec.ts
@@ -0,0 +1,38 @@
+import { FacebookAccount } from '@/domain/models'
+
+describe('FacebookAccount', () => {
+  const fbData = {
+    name: 'any-fb-name',
+    email: 'any-fb-email',
+    facebookId: 'any-fb-id'
+  }
+  test('should create with facebook data only', () => {
+    const sut = new FacebookAccount(fbData)
+
+    expect(sut).toEqual(fbData)
+  })
+
+  test('should update name if is empty', () => {
+    const accountData = { id: 'any-id' }
+    const sut = new FacebookAccount(fbData, accountData)
+
+    expect(sut).toEqual({
+      id: 'any-id',
+      name: 'any-fb-name',
+      email: 'any-fb-email',
+      facebookId: 'any-fb-id'
+    })
+  })
+
+  test('should not update name if is not empty', () => {
+    const accountData = { id: 'any-id', name: 'any-name' }
+    const sut = new FacebookAccount(fbData, accountData)
+
+    expect(sut).toEqual({
+      id: 'any-id',
+      name: 'any-name',
+      email: 'any-fb-email',
+      facebookId: 'any-fb-id'
+    })
+  })
+})
